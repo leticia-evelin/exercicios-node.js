08db0d32feaa4e187ea8b9e9712b9010abe67105
Leticia Evelin <ljkeleticia@gmail.com>
2023-02-12 22:53:13 -0300

    continuacao

--- exercicio01/app.js
+++ exercicio01/app.js
@@ -5,7 +5,12 @@
  * Versão: 1.0
  *****************************************************************/
 
- var media = require('./modulo/media');
+ var media = require('./modulo/media.js')
+
+//  const calculo = require('./modulo/media.js')
+const exame = require('./modulo/media.js')
+//  const relatorio = require('./modulo/media.js');
+
  var readline = require('readline');
 
  var entradaDados = readline.createInterface({
@@ -15,41 +20,81 @@
 
 
 entradaDados.question('Digite o gênero do(a) aluno(a) \n', function(genero1){
-   
-    entradaDados.question('Qual o nome do(a) aluno(a) ? \n', function(nome1){
+   let generoAluno = genero1.toUpperCase();
 
+   entradaDados.question('Qual o nome do(a) aluno(a) ? \n', function(nome1){
+        let nomeAluno = nome1;
+        
         entradaDados.question('Digite o gênero do(a) professor(a) \n', function(genero2){
+            let generoProfessor = genero2.toUpperCase();
 
             entradaDados.question('Qual o nome do(a) professor(a) ? \n', function(nome2){
-  
+                let nomeProfessor = nome2;
+
+
                 entradaDados.question('Qual o nome do curso? \n', function(curso){
+                    let nomeCurso = curso;
 
-                    entradaDados.question('Qual o nome da disciplina? \n', function(disciplina){
+                        entradaDados.question('Qual o nome da disciplina? \n', function(disciplina){
+                        let nomeDisciplina = disciplina;
 
-                        entradaDados.question('Digite a nota 1: \n', function(nota1){
+                            entradaDados.question('Digite a primeira nota de 0 a 100: \n', function(nota1){
+                            let primeiraNota = nota1;
 
-                            entradaDados.question('Digite a nota 2: \n', function(nota2){
+                                entradaDados.question('Digite a segunda nota de 0 a 100: \n', function(nota2){
+                                let segundaNota = nota2;
 
-                                entradaDados.question('Digite a nota 3: \n', function(nota3){
+                                    entradaDados.question('Digite a terceira nota de 0 a 100: \n', function(nota3){
+                                    let terceiraNota = nota3;
 
-                                    entradaDados.question('Digite a nota 4: \n', function(nota4){
+                                        entradaDados.question('Digite a quarta nota de 0 a 100: \n', function(nota4){
+                                        let quartaNota = nota4;
 
+                                        // Validação de entradas vazias
+                                         if(generoAluno == '' || nomeAluno == '' || generoProfessor == '' || nomeProfessor == '' || nomeCurso == '' || nomeDisciplina == '' || primeiraNota == '' || segundaNota == '' || terceiraNota == '' || quartaNota == '')
+                                         {
+                                             console.log('ERRO: É necessário preencher todos os campos de dados.')
+                                             entradaDados.close;
+                                         }if(primeiraNota < 0 || primeiraNota > 100 || segundaNota < 0 || segundaNota > 100 || terceiraNota < 0 || terceiraNota > 100 || quartaNota < 0 || quartaNota > 100) 
+                                         {
+                                             console.log('ERRO: O sistema aceita somente notas de 0 até 100.')
+                                             entradaDados.close;
+                                           }
 
+                                        else{
+                                            resultado = media.calculo(primeiraNota, segundaNota, terceiraNota, quartaNota);
+
+                                            if(resultado >= 70){
+                                                console.log('Status do Aluno: Aprovado :)')
+                                                entradaDados.close;
+
+                                            }else if (resultado < 50){
+                                                console.log('Status do Aluno: Reprovado :(')
+                                            }console.log('Média final: ' + resultado);
+                                              entradaDados.close; 
+                                         
+                                        }
+
+                                        if(resultado >= 50 && resultado < 69){
+                                            entradaDados.question('Digite a nota do exame: \n', function(notaExame){
+                                            let exame = notaExame;
+                                            })
+                                         
+                                        }else if (resultadoExame >= 60){
+                                            console.log('Aprovado no exame :)')
+
+                                        }else(resultado < 50)
+                                            console.log('Reprovado no exame :(')
+                                        
+                                            
+                                        
                                            
-                                        // Validações 
-                                       
-                                            if(generoAluno == '' || nomeAluno == '' || generoProfessor == '' || nomeProfessor == '' || nomeCurso == '' || nomeDisciplina == '' || primeiraNota == '' || segundaNota == '' || terceiraNota == '' || quartaNota == '')
-                                            {
-                                                console.log('ERRO: É necessário preencher todas as entradas.')
-                                            }else if(isNaN(primeiraNota) || isNaN(segundaNota) || isNaN(terceiraNota) || isNaN(quartaNota)){
-                                                console.log('ERRO: É necessários que todas as notas digitadas sejam números.')
-                                            }else if(isNaN(generoAluno) != isNaN(nomeAluno) != isNaN(generoProfessor) != isNaN(nomeProfessor) != isNaN(nomeCurso) != isNaN(nomeDisciplina))
-                                            {
-                                                console.log('ERRO: Apenas digite números nas notas. ')
-                                            }
-                                            console.log('Média final: ' + media);  
+                                        //console.log('Média final: ' + resultado); 
+                                        
 
-
+                                        // Chama a função para calcular, encaminhando os dados para o cálculo
+                                        //resultado = media.calculo(primeiraNota, segundaNota, terceiraNota, quartaNota);
+                                        //console.log('Média final: ' + resultado);
 
 
 
--- exercicio01/modulo/media.js
+++ exercicio01/modulo/media.js
@@ -5,25 +5,42 @@
  * Versão: 1.0
  *****************************************************************/
 
-const calculo = function(nome1, genero1, nome2, genero2, curso, disciplina, nota1, nota2, nota3, nota4){
+const calculo = function(nota1, nota2, nota3, nota4){
 
+    let primeiraNota = nota1;
+    let segundaNota = nota2;
+    let terceiraNota = nota3;
+    let quartaNota = nota4;
+    let media = (Number(primeiraNota) + Number(segundaNota) + Number(terceiraNota) + Number(quartaNota)) /4;
+    return media;
+
+}
+    
+    
+
+const exame = function(mediaFinal, notaExame){
+    let media = mediaFinal;
+    let exame = notaExame;
+
+    let resultadoExame = (Number(media) + Number(exame)) / 2
+
+    return resultadoExame;
+    
+}
+
+const relatorio = function(nome1, genero1, nome2, genero2, curso, disciplina){
     let nomeAluno = nome1;
     let generoAluno = genero1;
     let nomeProfessor = nome2;
     let generoProfessor = genero2;
     let nomeCurso = curso;
     let nomeDisciplina = disciplina;
-    let primeiraNota = nota1;
-    let segundaNota = nota2;
-    let terceiraNota = nota3;
-    let quartaNota = nota4;
-    let media = 0;
-    let mediaExame = 0;
 
-
-   media = (Number(primeiraNota) + Number(segundaNota) + Number(terceiraNota) + Number(quartaNota)) /4;
+   
 }
 
 module.exports = {
-    calculo
+    calculo,
+    exame,
+    relatorio
 }
