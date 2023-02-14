4d164575d18721615d7dc77a3d94e0e781043aa7
Leticia Evelin <ljkeleticia@gmail.com>
2023-02-14 17:05:23 -0300

    corrigir erro do 0

--- exercicio02/app.js
+++ exercicio02/app.js
@@ -17,43 +17,50 @@ var entradaDados = readline.createInterface({
 console.log('\n*******************TABUADAS*******************\n')
 
 entradaDados.question('Digite o número da inicial da tabuada [ENTRE 2 E 100]: \n', function(multiplicandoInicial){
-    let multiplicando = Number(multiplicandoInicial.replace(',' , '.'))
+    let multiplicando = Number(multiplicandoInicial.replace(',' , '.'));
 
     entradaDados.question('Digite o número final da tabuada [ENTRE 2 E 100]: \n', function(multiplicandoFinal){
-        let multiplicandoF = Number(multiplicandoFinal.replace(',' , '.'))
+        let multiplicandoF = Number(multiplicandoFinal.replace(',' , '.'));
 
         entradaDados.question('Digite o número inicial do contador: \n', function(contInicial){
-            let contadorInicial = Number(contInicial.replace(',' , '.'))
+            let contadorInicial = Number(contInicial.replace(',' , '.'));
 
             entradaDados.question('Digite o número final do contador: \n', function(contFinal){
-                let contadorFinal = contFinal.replace(',' , '.')
+                let contadorFinal = Number (contFinal.replace(',' , '.'));
 
-                if (multiplicando > multiplicandoF || multiplicandoF < multiplicando) {
-                    console.log('ERRO: O número inicial não pode ser maior que o número final')
-                    entradaDados.close()
+                if(Number (multiplicando) == '' || Number (multiplicandoF) == '' || Number (contadorInicial) == '' || Number (contadorFinal) == ''){
+                    console.log('ERRO: É necessário inserir valores em todas as entradas.');
+                    entradaDados.close();
+                    
+                } else if (multiplicando > multiplicandoF || multiplicandoF < multiplicando) {
+                    console.log('ERRO: O número inicial não pode ser maior que o número final');
+                    entradaDados.close();
         
                 } else if (multiplicando < 2 || multiplicando > 100 || multiplicandoF < 2 || multiplicandoF > 100) {
-                    console.log('ERRO: Digite apenas números de 2 até 100')
-                    entradaDados.close()
+                    console.log('ERRO: Digite apenas números de 2 até 100');
+                    entradaDados.close();
         
+                } else if(multiplicando == 0 || multiplicandoF == 0){
+                    console.log('ERRO: O sistema não pode calcular com valor 0.');
+                   entradaDados.close();
                 } else {
                     entradaDados.question('Digite o número do contador inicial da tabuada [ENTRE 1 E 50]: \n', function(contInicial){
-                        let contadorInicial = Number(contInicial.replace(',' , '.'))
+                        let contadorInicial = Number(contInicial.replace(',' , '.'));
 
                         entradaDados.question('Digite o número do contador final da tabuada [ENTRE 1 E 50]: \n', function(contFinal){
-                            let contadorFinal = Number(contFinal.replace(',' , '.'))
+                            let contadorFinal = Number(contFinal.replace(',' , '.'));
 
                             if (contadorInicial > contadorFinal || contadorFinal < contadorInicial) {
                                 console.log('ERRO: O número inicial não pode ser maior do que o final')
-                                entradaDados.close()
+                                entradaDados.close();
 
                             } else if (contadorInicial < 1 || contadorFinal > 50) {
                                 console.log('ERRO: Digite apenas números de 1 até 50')
-                                entradaDados.close()
+                                entradaDados.close();
         
                             } else {
                                 matematica.tabuada(multiplicando, multiplicandoF, contadorInicial, contadorFinal)
-                                entradaDados.close()
+                                entradaDados.close();
                             }
                         });
                     });
--- exercicio02/modulo/tabuada.js
+++ exercicio02/modulo/tabuada.js
@@ -10,25 +10,23 @@ const tabuada = function(multiplicandoInicial, multiplicandoFinal, contInicial, 
     let multiplicandoF = Number(multiplicandoFinal);
     let contadorInicial = Number(contInicial);
     let contadorFinal = Number(contFinal);
-    // let contTabuada = 0;
     let resultado;
-    let status = true; 
 
 
-    if(multiplicando == 0 || multiplicandoF == 0 || contadorFinal == 0){
-        console.log('ERRO: O sistema não pode calcular com valor 0.')
-        status = false;
-    }
+    // if(multiplicando == 0 || multiplicandoF == 0 || contadorFinal == 0){
+    //     console.log('ERRO: O sistema não pode calcular com valor 0.')
+    //     status = false;
+    // }
         
-    else if (isNaN(multiplicando) || isNaN(multiplicandoF) || isNaN(contadorInicial) || isNaN(contadorFinal)){
-        console.log('ERRO: O sistema só calcula com números.')
-        status = false;
-    } 
-    else if(multiplicando == '' || multiplicandoF == '' || contadorInicial == '' || contadorFinal == ''){
-        console.log('ERRO: É necessário inserir valores em todas as entradas.')
-        status = false;
+    // else if (isNaN(multiplicando) || isNaN(multiplicandoF) || isNaN(contadorInicial) || isNaN(contadorFinal)){
+    //     console.log('ERRO: O sistema só calcula com números.')
+    //     status = false;
+    // } 
+    // // else if(multiplicando == '' || multiplicandoF == '' || contadorInicial == '' || contadorFinal == ''){
+    //     console.log('ERRO: É necessário inserir valores em todas as entradas.')
+    //     status = false;
         
-    }
+    // }
 
     while (multiplicando <= multiplicandoF) {
         console.log(`\n************* Tabuada do ${multiplicando} ************* \n`)
