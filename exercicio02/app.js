c741b7bc0f37ddd5ea0d5305a88d0a2b24f169b3
LETÍCIA EVELIN AGUIAR E SILVA <22282118@CFP127ALUNO.LOCAL>
2023-02-17 10:58:56 -0300

    ok dnv

--- exercicio02/app.js
+++ exercicio02/app.js
@@ -16,39 +16,39 @@ var entradaDados = readline.createInterface({
 
 console.log('\n*******************TABUADAS*******************\n')
 
-entradaDados.question('Digite o número da inicial da tabuada [ENTRE 2 E 100]: \n', function(multiplicandoInicial){
-    let multiplicando = Number(multiplicandoInicial.replace(',' , '.'));
+entradaDados.question('Digite o número da inicial da tabuada [ENTRE 2 E 100]: \n', function (multiplicandoInicial) {
+    let multiplicando = Number(multiplicandoInicial.replace(',', '.'));
 
-    entradaDados.question('Digite o número final da tabuada [ENTRE 2 E 100]: \n', function(multiplicandoFinal){
-        let multiplicandoF = Number(multiplicandoFinal.replace(',' , '.'));
+    entradaDados.question('Digite o número final da tabuada [ENTRE 2 E 100]: \n', function (multiplicandoFinal) {
+        let multiplicandoF = Number(multiplicandoFinal.replace(',', '.'));
 
-        entradaDados.question('Digite o número inicial do contador: \n', function(contInicial){
-            let contadorInicial = Number(contInicial.replace(',' , '.'));
+        entradaDados.question('Digite o número inicial do contador: \n', function (contInicial) {
+            let contadorInicial = Number(contInicial.replace(',', '.'));
 
-            entradaDados.question('Digite o número final do contador: \n', function(contFinal){
-                let contadorFinal = Number (contFinal.replace(',' , '.'));
+            entradaDados.question('Digite o número final do contador: \n', function (contFinal) {
+                let contadorFinal = Number(contFinal.replace(',', '.'));
 
-                if(Number (multiplicando) == '' || Number (multiplicandoF) == '' || Number (contadorInicial) == '' || Number (contadorFinal) == ''){
+                if (Number(multiplicando) == '' || Number(multiplicandoF) == '' || Number(contadorInicial) == '' || Number(contadorFinal) == '') {
                     console.log('ERRO: É necessário inserir valores em todas as entradas.');
                     entradaDados.close();
-                    
+
                 } else if (multiplicando > multiplicandoF || multiplicandoF < multiplicando) {
                     console.log('ERRO: O número inicial não pode ser maior que o número final');
                     entradaDados.close();
-        
+
                 } else if (multiplicando < 2 || multiplicando > 100 || multiplicandoF < 2 || multiplicandoF > 100) {
                     console.log('ERRO: Digite apenas números de 2 até 100');
                     entradaDados.close();
-        
-                } else if(multiplicando == 0 || multiplicandoF == 0){
-                    console.log('ERRO: O sistema não pode calcular com valor 0.');
-                   entradaDados.close();
-                } else {
-                    entradaDados.question('Digite o número do contador inicial da tabuada: \n', function(contInicial){
-                        let contadorInicial = Number(contInicial.replace(',' , '.'));
 
-                        entradaDados.question('Digite o número do contador final da tabuada [ENTRE 1 E 50]: \n', function(contFinal){
-                            let contadorFinal = Number(contFinal.replace(',' , '.'));
+                } else if (multiplicando == 0 || multiplicandoF == 0) {
+                    console.log('ERRO: O sistema não pode calcular com valor 0.');
+                    entradaDados.close();
+                } else {
+                    entradaDados.question('Digite o número do contador inicial da tabuada: \n', function (contInicial) {
+                        let contadorInicial = Number(contInicial.replace(',', '.'));
+
+                        entradaDados.question('Digite o número do contador final da tabuada [ENTRE 1 E 50]: \n', function (contFinal) {
+                            let contadorFinal = Number(contFinal.replace(',', '.'));
 
                             if (contadorInicial > contadorFinal || contadorFinal < contadorInicial) {
                                 console.log('ERRO: O número inicial não pode ser maior do que o final')
@@ -57,7 +57,7 @@ entradaDados.question('Digite o número da inicial da tabuada [ENTRE 2 E 100]: \
                             } else if (contadorInicial < 0 || contadorFinal > 50) {
                                 console.log('ERRO: Digite apenas números de 1 até 50')
                                 entradaDados.close();
-        
+
                             } else {
                                 matematica.tabuada(multiplicando, multiplicandoF, contadorInicial, contadorFinal)
                                 entradaDados.close();
@@ -66,7 +66,7 @@ entradaDados.question('Digite o número da inicial da tabuada [ENTRE 2 E 100]: \
                     });
 
                 }
-                    
+
             });
         });
     });
